1fd54af5af69ffa2e6fc01c9fdf5f00a02a40f70
Ivan Ponomarev <iponomarev@mail.ru>
2022-02-17 21:22:45 +0300

    support PATCH

--- src/main/java/ru/curs/hurdygurdy/KotlinAPIExtractor.kt
+++ src/main/java/ru/curs/hurdygurdy/KotlinAPIExtractor.kt
@@ -15,6 +15,7 @@ import io.swagger.v3.oas.models.parameters.Parameter
 import io.swagger.v3.oas.models.parameters.RequestBody
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.PutMapping
@@ -144,6 +145,7 @@ class KotlinAPIExtractor(
             PathItem.HttpMethod.GET -> GetMapping::class
             PathItem.HttpMethod.POST -> PostMapping::class
             PathItem.HttpMethod.PUT -> PutMapping::class
+            PathItem.HttpMethod.PATCH -> PatchMapping::class
             PathItem.HttpMethod.DELETE -> DeleteMapping::class
             else -> null
         }
