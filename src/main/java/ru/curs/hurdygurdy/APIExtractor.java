f04f0b6555669477637c749727f411627e8e551a
Shishkin Sergey <shishkinsergeydmitrievich@gmail.com>
2024-08-20 20:34:26 +0300

    make controller interface public

--- src/main/java/ru/curs/hurdygurdy/APIExtractor.java
+++ src/main/java/ru/curs/hurdygurdy/APIExtractor.java
@@ -1,5 +1,6 @@
 package ru.curs.hurdygurdy;
 
+import com.squareup.javapoet.TypeSpec;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.Operation;
 import io.swagger.v3.oas.models.PathItem;
@@ -8,6 +9,7 @@ import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.parameters.Parameter;
 
+import javax.lang.model.element.Modifier;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -68,7 +70,11 @@ public abstract class APIExtractor<T, B> implements TypeSpecExtractor<T> {
                     operationId = CaseUtils.pathToCamel(stringPathItemEntry.getKey())
                             + CaseUtils.snakeToCamel(operationEntry.getKey().name().toLowerCase(), true);
                 }
-                buildMethod(openAPI, builder(typeName), stringPathItemEntry,
+                B b = builder(typeName);
+                if ("Controller".equals(name) && b instanceof TypeSpec.Builder) {
+                    ((TypeSpec.Builder) b).addModifiers(Modifier.PUBLIC);
+                }
+                buildMethod(openAPI, b, stringPathItemEntry,
                         operationEntry, operationId, responseParameter);
             }
         }
